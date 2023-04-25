d2359495657e3a112ebe7d9a845e9d92ae45f465
Mari <masha.vinogradova82@gmail.com>
2023-04-25 19:26:54 +0300

    Added timer

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+// tabs
 window.addEventListener('DOMContentLoaded', () => {
   let tabs = document.querySelectorAll('.tabheader__item');
   let tabsContent = document.querySelectorAll('.tabcontent');
@@ -35,4 +36,57 @@ window.addEventListener('DOMContentLoaded', () => {
       });
     }
   });
+  // Timer
+
+  const deadLine = '2023-05-30';
+
+  function getTimeRemaining(endtime) {
+    const t = Date.parse(endtime) - Date.parse(new Date());
+    const days = Math.floor(t / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+    const minutes = Math.floor((t / 1000 / 60) % 60);
+    const seconds = Math.floor((t / 1000) % 60);
+
+    return {
+      total: t,
+      days: days,
+      hours: hours,
+      minutes: minutes,
+      seconds: seconds,
+    };
+  }
+
+  function getZero(num) {
+    if (num >= 0 && num < 10) {
+      return `0${num}`;
+    } else {
+      return num;
+    }
+  }
+
+  function setClock(selector, endtime) {
+    const timer = document.querySelector(selector);
+    const days = timer.querySelector('#days');
+    const hours = timer.querySelector('#hours');
+    const minutes = timer.querySelector('#minutes');
+    const seconds = timer.querySelector('#seconds');
+    const timeInterval = setInterval(updateClock, 1000);
+
+    updateClock();
+
+    function updateClock() {
+      const t = getTimeRemaining(endtime);
+
+      days.textContent = getZero(t.days);
+      hours.textContent = getZero(t.hours);
+      minutes.textContent = getZero(t.minutes);
+      seconds.textContent = getZero(t.seconds);
+
+      if (t.total <= 0) {
+        clearInterval(timeInterval);
+      }
+    }
+  }
+
+  setClock('.timer', deadLine);
 });
