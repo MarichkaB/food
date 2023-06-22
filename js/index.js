467c0db64133d2a0661f7300b2ceacd4082a1bf4
Mari <masha.vinogradova82@gmail.com>
2023-06-22 15:56:06 +0300

    fixed classes for table

--- js/index.js
+++ js/index.js
@@ -161,6 +161,7 @@ window.addEventListener('DOMContentLoaded', () => {
       this.title = title;
       this.descr = descr;
       this.price = price;
+      this.classes = classes;
       this.parent = document.querySelector(parentSelector);
       this.transfer = 27;
       this.changeToUAH();
@@ -172,8 +173,13 @@ window.addEventListener('DOMContentLoaded', () => {
 
     render() {
       const element = document.createElement('div');
+      if (this.classes.length === 0) {
+        this.classes = 'menu__item';
+        element.classList.add(this.classes);
+      } else {
+        this.classes.forEach(className => element.classList.add(className));
+      }
       element.innerHTML = `  
-      <div class="menu__item">
         <img src=${this.src} alt=${this.alt}>
           <h3 class="menu__item-subtitle">${this.title}</h3>
             <div class="menu__item-descr">${this.descr}</div>
@@ -182,7 +188,6 @@ window.addEventListener('DOMContentLoaded', () => {
             <div class="menu__item-cost">Цена:</div>
           <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
       </div>
-  </div>
   `;
       this.parent.append(element);
     }
